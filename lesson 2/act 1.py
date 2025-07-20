f1b56320a2aac720eb9d19910ded40dc9c9883b2
jainkush1 <kushsuniljain@gmail.com>
2025-07-20 11:05:03 +0530

    commit

---
+++ lesson 2/act 1.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered is",n)
+    iteration=+1
+    print("Total illustration done by the code is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code won't change")
