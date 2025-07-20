2dc4656ea1e60fcaf1b178c8e2aa735da5bd9dee
jainkush1 <kushsuniljain@gmail.com>
2025-07-20 11:16:24 +0530

    commit

---
+++ lesson 1/act 2.py
@@ -0,0 +1,11 @@
+def ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when n is",n,"iterations",iteration)
+
+ontime(10)
+ontime(20)
+ontime(42)
+
+print("\n With every 'n' the time taken and illustrations will increase")
