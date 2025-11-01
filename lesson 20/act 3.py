4f3f04eaba47e028536df14d6a067f61d22206ff
jainkush1 <kushsuniljain@gmail.com>
2025-11-01 17:23:28 +0530

    commit

--- lesson 20/act 3.py
+++ lesson 20/act 3.py
@@ -5,5 +5,5 @@ def leaders(a, a_size):
         if currentmax < a[i]:
             print(a[i])
             currentmax = a[i]
-a=[116, 17, 4, 3, 5, 2451]
+a=[7116, 17, 4000, 3, 5, 2451]
 leaders(a,len(a))
